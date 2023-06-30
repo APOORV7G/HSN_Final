7e52e2351e54ef3f8f09ce7f04aa1cb9e032234a
APOORV7G <130035517+APOORV7G@users.noreply.github.com>
2023-06-30 22:51:23 +0530

    Frame2
    Added the second Frame to the main Frame which accepts the cell range

--- MyProject/src/Frame2.java
+++ MyProject/src/Frame2.java
@@ -1,10 +1,34 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
 public class Frame2 extends JFrame {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextArea txtrFrom;
+	private JTextArea txtrTo;
 
 	/**
 	 * Launch the application.
@@ -26,17 +50,153 @@ public class Frame2 extends JFrame {
 	 * Create the application.
 	 */
 	public Frame2() {
+		
 		initialize();
+		
 	}
+
+	
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @param s 
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 561, 483);
+		frame = new JFrame("HSN");
+		frame.setBounds(100, 100, 560, 460);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-	}
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(142, 42, 183, 32);
+		frame.getContentPane().add(panel);
+		
+		JTextArea txtrEnterTheCell = new JTextArea();
+		txtrEnterTheCell.setText("Enter the cell range :");
+		txtrEnterTheCell.setEditable(false);
+		panel.add(txtrEnterTheCell);
+		
+		textField = new JTextField();
+		textField.setBorder(new LineBorder(new Color(0, 0, 0)));
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setBounds(239, 128, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		textField_1.setSelectionColor(new Color(0, 0, 0));
+		textField_1.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_1.setBounds(239, 85, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		txtrFrom = new JTextArea();
+		txtrFrom.setText("FROM:");
+		txtrFrom.setEditable(false);
+		txtrFrom.setBounds(142, 83, 87, 22);
+		frame.getContentPane().add(txtrFrom);
+		
+		txtrTo = new JTextArea();
+		txtrTo.setText("To:");
+		txtrTo.setEditable(false);
+		txtrTo.setBounds(142, 126, 87, 22);
+		frame.getContentPane().add(txtrTo);
+		
+		JButton submit = new JButton("Submit");
+		submit.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				if(e.getSource()==submit) 
+				{
+					
+						if(textField.getText().trim().isEmpty() || textField_1.getText().trim().isEmpty() ||textField.getText().contentEquals(textField_1.getText())
+								)
+						{  
+							System.out.println("Invalid name");
+							
+							
+						}
+						else
+						{
+							try 
+							{
+					            BufferedReader reader = new BufferedReader(new FileReader("temporary.txt"));
+					            String line;
+					            
+					            line = reader.readLine();
+					            reader.close();
+					                System.out.println(line);
+					                {
+					                try (FileInputStream inputStream = new FileInputStream(line);
+					                        Workbook workbook = new XSSFWorkbook(inputStream)) 
+					                {
 
+					                       @SuppressWarnings("unused")
+										Sheet sheet = workbook.getSheet("Data");
+					                       
+										
+					                          /* 
+					                           * Change this with the curretn values
+					                           * 
+					                           * Row row = sheet.createRow(1);
+					                           String rowValues="A,B,C,D,E";
+					                          
+
+					                           String[] values = rowValues.split(",");
+					                           for (int j = 0; j < values.length; j++) 
+					                           {
+					                               Cell cell = row.createCell(j);
+					                               cell.setCellValue(values[j].trim());
+					                               
+					                           
+					                           }*/
+					                           FileOutputStream outputStream = new FileOutputStream(line);
+					                           workbook.write(outputStream);
+					                           outputStream.close();
+					                }catch (IOException e2) 
+					                {
+					                    e2.printStackTrace();
+					                }
+					                
+					           
+					                }
+					             }  
+					         
+							catch (IOException e1) {
+					            System.out.println("An error occurred while reading the file: " + e1.getMessage());
+					        }
+							
+						}
+				}           		
+			}
+		 }
+								);
+			
+		
+		
+		
+		submit.setBackground(new Color(255, 255, 255));
+		submit.setBorder(new LineBorder(new Color(0, 0, 0)));
+		submit.setBounds(142, 178, 89, 23);
+		frame.getContentPane().add(submit);
+		
+		JButton reset = new JButton("Reset");
+		reset.setBackground(new Color(255, 255, 255));
+		reset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(null);
+				textField_1.setText(null);
+			}
+		});
+		reset.setBorder(new LineBorder(new Color(0, 0, 0)));
+		reset.setBounds(239, 178, 89, 23);
+		frame.getContentPane().add(reset);
+		
+	}
 }
+		
--- MyProject/src/MainFrame.java
+++ MyProject/src/MainFrame.java
@@ -54,10 +54,10 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame("HSN");
-		frame.setForeground(new Color(255, 255, 255));
-		frame.setBackground(new Color(0, 0, 0));
-		frame.getContentPane().setBackground(new Color(0, 0, 0));
-		frame.getContentPane().setForeground(new Color(255, 255, 255));
+		frame.setForeground(new Color(0, 0, 0));
+		frame.setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setBackground(new Color(255, 255, 255));
+		frame.getContentPane().setForeground(new Color(0, 0, 0));
 		frame.setBounds(100, 100, 560, 460);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -65,8 +65,8 @@ public class MainFrame {
 		
 		
 		JPanel panel = new JPanel();
-		panel.setForeground(new Color(255, 255, 255));
-		panel.setBackground(new Color(0, 0, 0));
+		panel.setForeground(new Color(0, 0, 0));
+		panel.setBackground(new Color(255, 255, 255));
 		panel.setBounds(171, 11, 71, 34);
 		frame.getContentPane().add(panel);
 		panel.setLayout(new BorderLayout(0, 0));
@@ -74,13 +74,13 @@ public class MainFrame {
 		JLabel mainlabel = new JLabel("Welcome");
 		panel.add(mainlabel, BorderLayout.CENTER);
 		mainlabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		mainlabel.setForeground(new Color(255, 255, 255));
+		mainlabel.setForeground(new Color(0, 0, 0));
 		mainlabel.setBackground(new Color(0, 0, 0));
 		mainlabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JLabel mainlabel2 = new JLabel("Enter file name here:");
-		mainlabel2.setForeground(new Color(255, 255, 255));
-		mainlabel2.setBackground(new Color(0, 0, 0));
+		mainlabel2.setForeground(new Color(0, 0, 0));
+		mainlabel2.setBackground(new Color(255, 255, 255));
 		mainlabel2.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		mainlabel2.setHorizontalAlignment(SwingConstants.CENTER);
 		mainlabel2.setBounds(120, 116, 122, 20);
@@ -88,23 +88,23 @@ public class MainFrame {
 		
 		JTextField maintext = new JTextField();
 		maintext.setHorizontalAlignment(SwingConstants.CENTER);
-		maintext.setForeground(new Color(255, 255, 255));
+		maintext.setForeground(new Color(0, 0, 0));
 		maintext.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		maintext.setBackground(new Color(0, 0, 0));
+		maintext.setBackground(new Color(255, 255, 255));
 		maintext.setBounds(252, 116, 170, 20);
 		maintext.setText(null);
-		maintext.setBorder(new LineBorder(new Color(255, 255, 255)));
+		maintext.setBorder(new LineBorder(new Color(0, 0, 0)));
 		maintext.setBorder(BorderFactory.createCompoundBorder(maintext.getBorder(),BorderFactory.createEmptyBorder(2, 5, 2, 2)));
 		//maintext.setMargin(new Insets(2, 5, 2, 2));
-		maintext.setCaretColor(Color.WHITE);
+		maintext.setCaretColor(new Color(0, 0, 0));
 		frame.getContentPane().add(maintext);
 		maintext.setColumns(10);
 		
 		JButton mainbutton = new JButton("Submit");
 		mainbutton.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		mainbutton.setForeground(new Color(255, 255, 255));
-		mainbutton.setBackground(new Color(0, 0, 0));
-		mainbutton.setBorder(new LineBorder(new Color(255, 255, 255), 1, true));
+		mainbutton.setForeground(new Color(0, 0, 0));
+		mainbutton.setBackground(new Color(255, 255, 255));
+		mainbutton.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		JButton btnNewButton = new JButton("Reset");
 		mainbutton.addActionListener(new ActionListener() {
 			
@@ -133,6 +133,7 @@ public class MainFrame {
 			                    maintext.setEditable(false);
 			                    mainbutton.setEnabled(false);
 			                    btnNewButton.setEnabled(false);	
+			                    System.out.println(maintext.getText()+".xlsx created");
 			                    new Frame2();
 			                    frame.dispose();
 			                    try {
@@ -189,22 +190,22 @@ public class MainFrame {
 				 
 			}
 		}								);
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBackground(new Color(0, 0, 0));
+		btnNewButton.setForeground(new Color(0, 0, 0));
+		btnNewButton.setBackground(new Color(255, 255, 255));
 		btnNewButton.setBounds(252, 152, 61, 23);
-		btnNewButton.setBorder(new LineBorder(new Color(255, 255, 255), 1, true));
+		btnNewButton.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		frame.getContentPane().add(btnNewButton);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setForeground(Color.WHITE);
-		panel_1.setBackground(Color.BLACK);
+		panel_1.setForeground(new Color(0, 0, 0));
+		panel_1.setBackground(new Color(255, 255, 255));
 		panel_1.setBounds(252, 11, 35, 34);
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblUser = new JLabel("User");
 		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
-		lblUser.setForeground(Color.WHITE);
+		lblUser.setForeground(new Color(0, 0, 0));
 		lblUser.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		lblUser.setBackground(Color.BLACK);
 		panel_1.add(lblUser, BorderLayout.CENTER);
